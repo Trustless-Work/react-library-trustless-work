217471dcec49b0cf70e169901815201a38834619
Joel Vargas <vermudo.com@gmail.com>
2025-05-20 16:01:58 -0600

    feat: export useGetEscrow hook for retrieving escrow data

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { TrustlessWorkProvider } from "./provider";
 export { useInitializeEscrow } from "./hooks/useInitializeEscrow";
 export { useSendTransaction } from "./hooks/useSendTransaction";
+export { useGetEscrow } from "./hooks/useGetEscrow";
