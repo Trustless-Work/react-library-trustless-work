e2b3472f5718fc7e4b3bfa3d526b64bf56af5b7c
Joel Vargas <vermudo.com@gmail.com>
2025-05-21 11:06:30 -0600

    feat: add comprehensive type exports and documentation for Trustless Work API components

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
+/**
+ * Provider for the Trustless Work API
+ */
 export { TrustlessWorkProvider } from "./provider";
+
+/**
+ * Hooks for the Trustless Work API
+ */
 export { useInitializeEscrow } from "./hooks/useInitializeEscrow";
 export { useSendTransaction } from "./hooks/useSendTransaction";
 export { useGetEscrow } from "./hooks/useGetEscrow";
@@ -10,3 +17,43 @@ export { useReleaseFunds } from "./hooks/useReleaseFunds";
 export { useFundEscrow } from "./hooks/useFundEscrow";
 export { useChangeMilestoneStatus } from "./hooks/useChangeMilestoneStatus";
 export { useChangeMilestoneApprovedFlag } from "./hooks/useChangeMilestoneApprovedFlag";
+
+/**
+ * Types for the Trustless Work API
+ */
+export type { Escrow } from "./types.entity";
+export type { Milestone } from "./types.entity";
+export type { Roles } from "./types.entity";
+export type { Flags } from "./types.entity";
+export type { Trustline } from "./types.entity";
+
+/**
+ * Types for the Trustless Work API
+ */
+export type { EscrowRequestResponse } from "./types.response";
+export type { SendTransactionResponse } from "./types.response";
+export type { InitializeEscrowResponse } from "./types.response";
+export type { UpdateEscrowResponse } from "./types.response";
+export type { GetEscrowBalancesResponse } from "./types.response";
+
+/**
+ * Types for the Trustless Work API
+ */
+export type { baseURL } from "./types";
+export type { Status } from "./types";
+
+/**
+ * Types for the Trustless Work API
+ */
+export type { EscrowPayload } from "./types.payload";
+export type { InitializeEscrowPayload } from "./types.payload";
+export type { ChangeMilestoneStatusPayload } from "./types.payload";
+export type { ChangeMilestoneApprovedFlagPayload } from "./types.payload";
+export type { StartDisputePayload } from "./types.payload";
+export type { ResolveDisputePayload } from "./types.payload";
+export type { FundEscrowPayload } from "./types.payload";
+export type { GetEscrowPayload } from "./types.payload";
+export type { ReleaseFundsPayload } from "./types.payload";
+export type { UpdateEscrowPayload } from "./types.payload";
+export type { GetBalanceParams } from "./types.payload";
+export type { SendTransactionPayload } from "./types.payload";
