a1f5d1b12a05ba1cdcc72ae16e9d9f89e4aff8d5
Joel Vargas <vermudo.com@gmail.com>
2026-05-20 17:31:42 -0600

    feat: implement error handling for Trustless Work API with custom error classes and utility functions

---
+++ src/errors/index.ts
@@ -0,0 +1,8 @@
+export { TrustlessWorkApiError } from "./trustless-work-api-error";
+export {
+  formatApiErrorMessage,
+  isProblemDetails,
+  parseProblemDetails,
+  parseProblemDetailsFromAxiosError,
+  toTrustlessWorkError,
+} from "./parse-problem-details";
